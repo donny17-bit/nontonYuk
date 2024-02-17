664f2951ecb21e84849660c8ad5349e67000fff6
donny17-bit <doniwahyu14@gmail.com>
2024-02-17 22:02:45 +0700

    comment auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,13 @@ const Router = express.Router();
 const movieRoutes = require("../moduls/movie/movieRoutes");
 const scheduleRoutes = require("../moduls/schedule/scheduleRoutes");
 const bookingRoutes = require("../moduls/booking/bookingRoutes");
-const authRoutes = require("../moduls/auth/authRoutes");
+// const authRoutes = require("../moduls/auth/authRoutes");
 const userRoutes = require("../moduls/users/userRoutes");
 
 Router.use("/movie", movieRoutes);
 Router.use("/schedule", scheduleRoutes);
 Router.use("/booking", bookingRoutes);
-Router.use("/auth", authRoutes);
+// Router.use("/auth", authRoutes);
 Router.use("/user", userRoutes);
 
 module.exports = Router;
